5ef86206a711bd6e421a6e640babb3bfefb04d3a
Weston Schmidt <Weston_Schmidt@alumni.purdue.edu>
2017-02-06 09:53:26 -0800

    Updates from our meeting.

--- src/cjwt.h
+++ src/cjwt.h
@@ -19,13 +19,30 @@
 /*----------------------------------------------------------------------------*/
 /*                                   Macros                                   */
 /*----------------------------------------------------------------------------*/
-/* none */
+#define OPT_ALLOW_ALG_NONE  (1<<0)
+#define OPT_ALLOW_ANY_TIME  (1<<1)
 
 /*----------------------------------------------------------------------------*/
 /*                               Data Structures                              */
 /*----------------------------------------------------------------------------*/
+typedef enum {
+    alg_none,
+    alg_es256,
+    alg_es384,
+    alg_es512,
+    alg_hs256,
+    alg_hs384,
+    alg_hs512,
+    alg_ps256,
+    alg_ps384,
+    alg_ps512,
+    alg_rs256,
+    alg_rs384,
+    alg_rs512
+} jwa_alg_t;
+
 typedef struct {
-    char *alg
+    jwa_alg_t alg;
     char *kid
 
     /* Unsupported:
@@ -52,24 +69,39 @@ typedef struct {
     struct timespec iat;
     char *jti;
 
+    cJSON *private_claims;
+
+    void *internal_use_only;
 } cjwt_t
-
-/*----------------------------------------------------------------------------*/
-/*                            File Scoped Variables                           */
-/*----------------------------------------------------------------------------*/
-/* none */
-
-/*----------------------------------------------------------------------------*/
-/*                             Function Prototypes                            */
-/*----------------------------------------------------------------------------*/
-/* none */
 
 /*----------------------------------------------------------------------------*/
 /*                             External Functions                             */
 /*----------------------------------------------------------------------------*/
-/* none */
 
-/*----------------------------------------------------------------------------*/
-/*                             Internal functions                             */
-/*----------------------------------------------------------------------------*/
-/* none */
+/**
+ *  The function to use to decode and validate a JWT.
+ *
+ *  @note This function allocates memory associated with the output jwt that
+ *        must be freed.  cjwt_destroy() must be called to destry the object
+ *        when we are done with it.
+ *
+ *  @note This function does not
+ *
+ *  @param encoded [IN]  the incoming encoded JWT (MUST be '\0' terminated string)
+ *  @param options [IN]  a bitmask of the options
+ *  @param jwt     [OUT] the resulting JWT if found to be valid,
+ *                       set to NULL if not successful
+ *  @param key     [IN]  the public key to use for validating the signature
+ *  @param key_len [IN]  the length of the key in bytes
+ *
+ *  @retval   0 successful
+ *  @retval  -1 invalid jwt format
+ *  @retval  -2 mismatched key
+ *  ... etc
+ */
+int cjwt_decode( const char *encoded, unsigned int options, cjwt_t **jwt,
+                 const uint8_t *key, size_t key_len );
+
+/**
+ */
+int cjwt_destroy( cjwt_t **jwt );
